eb87cef9aaeae740b2680bd090ae66ce7fc470de
Fajri Arlin Nauli Rangkuti <fajriarlinnaulirangkuti@gmail.com>
2022-04-26 15:23:20 +0700

    [FIXING CONFLICT]

--- src/main/java/com/mandiri/controllers/PaylaterDetailController.java
+++ src/main/java/com/mandiri/controllers/PaylaterDetailController.java
@@ -4,10 +4,7 @@ import com.mandiri.entities.dtos.PaylaterDetailDto;
 import com.mandiri.entities.dtos.PaylaterSaveDto;
 import com.mandiri.entities.dtos.PaymentPerMonthDto;
 import com.mandiri.generator.PDFGenerator;
-<<<<<<< HEAD
-=======
 import com.mandiri.generator.PDFPayLaterMonthlyGenerator;
->>>>>>> 229a63257a49a27eea0f5cf8703187da4eaf8b2a
 import com.mandiri.services.PaylaterDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +15,11 @@ public class PaylaterDetailController {
     @Autowired
     PaylaterDetailService paylaterDetailService;
 
-<<<<<<< HEAD
     @Autowired
     PDFGenerator pdfGenerator;
-=======
-    //
+
     @Autowired
     PDFPayLaterMonthlyGenerator payLaterMonthlyGenerator;
->>>>>>> 229a63257a49a27eea0f5cf8703187da4eaf8b2a
-
     @PostMapping
     public PaylaterSaveDto save (@RequestBody PaylaterSaveDto detailDto){
         return paylaterDetailService.savePayment(detailDto);
@@ -42,16 +35,15 @@ public class PaylaterDetailController {
         return paylaterDetailService.update(perMonthDto);
     }
 
-<<<<<<< HEAD
     @GetMapping("/{id}/{imageName}/export")
-    public void getEmployeePdf(@PathVariable String id,@PathVariable String imageName){
-        pdfGenerator.generatePdfReport(id,imageName);
-=======
+    public void getEmployeePdf(@PathVariable String id,@PathVariable String imageName) {
+        pdfGenerator.generatePdfReport(id, imageName);
+    }
+
     //Update 26 April 2022
     @GetMapping("{id}/pdf")
     public void getPayLaterMonthly(@PathVariable String id){
         payLaterMonthlyGenerator.generatePdfReport(id);
->>>>>>> 229a63257a49a27eea0f5cf8703187da4eaf8b2a
     }
 
 }
