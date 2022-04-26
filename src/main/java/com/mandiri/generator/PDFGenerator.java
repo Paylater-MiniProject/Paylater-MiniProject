a19e896f5e22d6858791e1fb14451efbc8c158a5
Fajri Arlin Nauli Rangkuti <fajriarlinnaulirangkuti@gmail.com>
2022-04-26 11:43:26 +0700

    [GENERATE PDF]

--- src/main/java/com/mandiri/generator/PDFGenerator.java
+++ src/main/java/com/mandiri/generator/PDFGenerator.java
@@ -1,15 +1,20 @@
 package com.mandiri.generator;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Currency;
 import java.util.List;
 
+import com.mandiri.entities.dtos.PaylaterDetailDto;
 import com.mandiri.entities.models.Employee;
+import com.mandiri.entities.models.PaylaterDetail;
 import com.mandiri.repositories.EmployeeRepository;
 import com.mandiri.services.EmployeeService;
+import com.mandiri.services.PaylaterDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -26,6 +31,8 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import javax.swing.plaf.synth.ColorType;
+
 @Component
 public class PDFGenerator{
 
@@ -41,6 +48,9 @@ public class PDFGenerator{
     @Value("${logoImgPath}")
     private String logoImgPath;
 
+    @Value("${logoFooter}")
+    private String logoFooter;
+
     @Value("${logoImgScale}")
     private Float[] logoImgScale;
 
@@ -53,14 +63,12 @@ public class PDFGenerator{
     @Value("${table.columnNames}")
     private List<String> columnNames;
 
-    @Autowired
-    EmployeeRepository eRepo;
 
     @Autowired
-    EmployeeService employeeService;
+    PaylaterDetailService paylaterDetailService;
 
     private static Font COURIER = new Font(Font.FontFamily.HELVETICA, 20, Font.BOLD);
-    private static Font COURIER_SMALL = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLD);
+    private static Font COURIER_SMALL = new Font(Font.FontFamily.COURIER, 12, Font.NORMAL);
     private static Font COURIER_SMALL_FOOTER = new Font(Font.FontFamily.COURIER, 8, Font.BOLD);
 
     public void generatePdfReport(String id) {
@@ -98,18 +106,23 @@ public class PDFGenerator{
 
     public void addDocTitle(Document document,String id) throws DocumentException {
 
-        Employee employee = employeeService.getEmployee(id);
+        PaylaterDetailDto paylaterDetail = paylaterDetailService.getAllPayment(id);
 
         Paragraph p1 = new Paragraph();
-        leaveEmptyLine(p1, 1);
-        p1.add(new Paragraph("LIVIN PAYLATER", COURIER));
-        p1.setAlignment(Element.ALIGN_CENTER);
+        Paragraph title = new Paragraph();
         leaveEmptyLine(p1, 1);
 
-        p1.add(new Paragraph("Name: " + employee.getEmpName(), COURIER_SMALL));
-        p1.add(new Paragraph("Deparment: " + employee.getEmpDept(), COURIER_SMALL));
 
+        p1.add(new Paragraph("Name           : " + "Asep Julianta", COURIER_SMALL));
+        leaveEmptyLine(p1, 1);
+        p1.add(new Paragraph("Address        : " + "Jl.Kemanggisan no.90", COURIER_SMALL));
+        leaveEmptyLine(p1, 1);
+        p1.add(new Paragraph("Transaction id : " + paylaterDetail.getId(), COURIER_SMALL));
+
+
+        document.add(title);
         document.add(p1);
+
     }
 
     private void createTable(Document document, int noOfColumns,String id) throws DocumentException {
@@ -121,43 +134,64 @@ public class PDFGenerator{
 
         for(int i=0; i<noOfColumns; i++) {
             PdfPCell cell = new PdfPCell(new Phrase(columnNames.get(i)));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            cell.setBackgroundColor(BaseColor.CYAN);
+            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+            cell.setBackgroundColor(BaseColor.GRAY);
             table.addCell(cell);
         }
 
         table.setHeaderRows(1);
         getDbData(table,id);
+
+
+
         document.add(table);
     }
 
     private void getDbData(PdfPTable table,String id) {
 
-        Employee employee = employeeService.getEmployee(id);
+        PaylaterDetailDto paylaterDetailDto = paylaterDetailService.getAllPayment(id);
 
-            table.setWidthPercentage(100);
-            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.setWidthPercentage(100);
+        table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_LEFT);
+        table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.getDefaultCell().setBorder(2);
 
-            table.addCell(employee.getEmpId());
-            table.addCell(employee.getEmpName());
-            table.addCell(employee.getEmpDept());
-            table.addCell(currencySymbol + employee.getEmpSal().toString());
-
-            System.out.println(employee.getEmpName());
+        table.addCell("Cicilan");
+        table.addCell(String.valueOf(paylaterDetailDto.getCurrentInstallment() + 1));
+        table.addCell("Nama Produk");
+        table.addCell(paylaterDetailDto.getProductName());
+        table.addCell("Status Pembayaran");
+        table.addCell(paylaterDetailDto.getStatus());
+        table.addCell("Harga Product");
+        table.addCell(paylaterDetailDto.getPrice().toString());
+        table.addCell("Qty");
+        table.addCell(paylaterDetailDto.getTotalProduct().toString());
+        table.addCell("Biaya Admin");
+        table.addCell(paylaterDetailDto.getHandlingFee().toString());
+        table.addCell("Total Pembayaran");
+        table.addCell(paylaterDetailDto.getTransactionAmount().toString());
 
 
     }
 
     private void addFooter(Document document) throws DocumentException {
-        Paragraph p2 = new Paragraph();
-        leaveEmptyLine(p2, 3);
-        p2.setAlignment(Element.ALIGN_MIDDLE);
-        p2.add(new Paragraph(
-                "------------------------End Of " +reportFileName+"------------------------",
-                COURIER_SMALL_FOOTER));
+//        Paragraph p2 = new Paragraph();
+//        leaveEmptyLine(p2, 3);
+//        p2.setAlignment(Element.ALIGN_CENTER);
+//        p2.add(new Paragraph("Livin By Mandiri",
+//                COURIER_SMALL_FOOTER));
+//
+//        document.add(p2);
 
-        document.add(p2);
+        try {
+            Image img = Image.getInstance(logoFooter);
+            img.scalePercent(25, logoImgScale[1]);
+            img.setAlignment(Element.ALIGN_LEFT);
+            document.add(img);
+        } catch (DocumentException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     private static void leaveEmptyLine(Paragraph paragraph, int number) {
@@ -170,4 +204,5 @@ public class PDFGenerator{
         String localDateString = LocalDateTime.now().format(DateTimeFormatter.ofPattern(reportFileNameDateFormat));
         return pdfDir+reportFileName+"-"+localDateString+".pdf";
     }
+
 }
